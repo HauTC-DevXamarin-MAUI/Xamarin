8a40b19a03eb829c6636cf32dedbbf86345c4b11
CongHau <conghau0112@gmail.com>
2021-11-08 08:27:38 +0700

    C5 - upload code lan cuoi

--- Xamarin/Xamarin.Android/obj/Debug/110/android/environment.x86_64.s
+++ Xamarin/Xamarin.Android/obj/Debug/110/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"28ccfb32-89ab-49b8-b6b8-49f3ff54d98c"
+	.asciz	"fe1956a9-8291-4dc5-8efc-29293a07907a"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
